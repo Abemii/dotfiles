8f9b1ad6fc311cdb05ff29e93cb5da02470f1962
michiya.hc51@gmail.com <abemi@pec4130a.hpc.vpl.nii.ac.jp>
2018-07-15 23:49:03 +0900

    add if option for remote server

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -2,6 +2,6 @@
 hn=$(hostname)
 if [ `echo $hn | grep 'Mac'` ]; then
     ln -sf ~/dotfiles/nvim/config/ ~/.config/nvim
-else
-    ln -sf ~/dotfiles/nvim/config/ 
+elif [ `echo $hn | grep 'hpc.vpl.nii.ac.jp'` ]; then
+    ln -sf $EXT_HOME/dotfiles/nvim/config/ $EXT_HOME/.config/nvim 
 fi
