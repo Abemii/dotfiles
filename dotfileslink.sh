ec03e6cc5b641ccfe62690fd651d517ec6c16a04
Michiya Abe <michiya.hc51@gmail.com>
2018-07-16 13:39:20 +0900

    add fish_prompt.fish

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -2,6 +2,8 @@
 hn=$(hostname)
 if [ `echo $hn | grep 'Mac'` ]; then
     ln -sf ~/dotfiles/nvim/config/ ~/.config/nvim
+    ln -sf ~/dotfiles/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 elif [ `echo $hn | grep 'hpc.vpl.nii.ac.jp'` ]; then
     ln -sf $EXT_HOME/dotfiles/nvim/config/ $EXT_HOME/.config/nvim 
+    ln -sf $EXT_HOME/dotfiles/fish_prompt.fish $EXT_HOME/.config/fish/functions/fish_prompt.fish
 fi
