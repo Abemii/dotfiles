b98c56d607643c4af9ff13c9e4cc14aba78fff46
Michiya Abe <michiya.hc51@gmail.com>
2018-08-20 01:44:34 +0900

    zsh

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -5,5 +5,5 @@ if [ `echo $hn | grep 'Mac'` ]; then
     ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 elif [ `echo $hn | grep 'hpc'` ]; then
     ln -sf $EXT_HOME/dotfiles/nvim/config/ $EXT_HOME/.config/nvim 
-    ln -sf $EXT_HOME/dotfiles/zsh/.zshrc $EXT_HOME/.zshrc
+    ln -sf $EXT_HOME/dotfiles/zsh/.zshrc ~/.zshrc
 fi
