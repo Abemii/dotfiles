16f6f136661acabb95bc55b4a843fee5b51ab533
Michiya Abe <michiya.hc51@gmail.com>
2018-08-20 10:17:03 +0900

    modify for jupyter

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -8,8 +8,10 @@ elif [ `echo $hn | grep 'hpc.vpl.nii.ac.jp'` ]; then
     ln -sf $EXT_HOME/dotfiles/.bash_profile ~/.bash_profile
     ln -sf $EXT_HOME/dotfiles/.zshrc ~/.zshrc
     ln -sf $EXT_HOME/dotfiles/.tmux.conf ~/.tmux.conf
+    ln -sf $EXT_HOME/dotfiles/.jupyter ~/
 elif [ `echo $hn | grep 'ubuntu'` ]; then
     ln -sf ~/dotfiles/nvim/config/ ~/.config/nvim
     ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
     ln -sf ~/dotfiles/.zshrc ~/.zshrc
+    ln -sf ~/dotfiles/.jupyter ~/
 fi
