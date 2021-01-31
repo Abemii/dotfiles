b36afbd6ec0e3f82b1fc8f7f6c8c909256214b1a
Michiya Abe <michiya.hc51@gmail.com>
2021-02-01 01:16:00 +0900

    update quicktile cfgs and setup.sh

--- setup.sh
+++ setup.sh
@@ -486,4 +486,26 @@ if ! { type ag > /dev/null 2>&1; } then
 fi
 
 
+# install filezilla
+echo -e "\e[32mInstall htop\e[m"
+if ! { type htop > /dev/null 2>&1; } then
+    if $IS_LINUX && $IS_SUDOER; then
+        sudo apt install filezilla
+    elif $IS_MAC; then
+        brew install filezilla
+    fi
+fi
+
+
+# install quicktile
+echo -e "\e[32mInstall htop\e[m"
+if ! { type quicktile > /dev/null 2>&1; } then
+    if $IS_LINUX && $IS_SUDOER; then
+        sudo apt-get install python3 python3-pip python3-setuptools python3-gi python3-xlib python3-dbus gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-wnck-3.0
+        sudo pip3 install https://github.com/ssokolow/quicktile/archive/master.zip
+        ln -s $(pwd)/quicktile.cfg ~/.config/
+    fi
+fi
+
+
 echo -e "\e[32mFinished\e[m"
