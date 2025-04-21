e887d51a69adf15d3a09e32db37b94b5930625fc
Michiya Abe <michiya.hc51@gmail.com>
2025-04-21 23:22:52 +0900

    use copilot

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -4,28 +4,44 @@ return {
         event = "VeryLazy",
         version = false, -- Never set this value to "*"! Never!
         opts = {
-            -- add any opts here
-            -- for example
-            provider = "claude",
-            -- auto_suggestions_provider = "copilot",
+            -- -- add any opts here
+            -- -- for example
+            -- provider = "claude",
+            -- -- auto_suggestions_provider = "copilot",
 
-            -- openai = {
-            --     endpoint = "https://api.openai.com/v1",
-            --     model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-            --     timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-            --     temperature = 0,
-            --     max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-            --     --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+            -- -- openai = {
+            -- --     endpoint = "https://api.openai.com/v1",
+            -- --     model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+            -- --     timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+            -- --     temperature = 0,
+            -- --     max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            -- --     --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+            -- -- },
+            -- behaviour = {
+            --     enable_cursor_planning_mode = true, -- Whether to enable Cursor Planning Mode. Default to false.
             -- },
-            behaviour = {
-                enable_cursor_planning_mode = true, -- Whether to enable Cursor Planning Mode. Default to false.
+            -- claude = {
+            --     endpoint = "https://api.anthropic.com",
+            --     model = "claude-3-7-sonnet-20250219",
+            --     temperature = 0,
+            --     max_tokens = 4096,
+            --     -- disable_tools = true,
+            -- },
+            provider = "copilot",
+            copilot = {
+                model = "claude-3.7-sonnet",
             },
-            claude = {
-                endpoint = "https://api.anthropic.com",
-                model = "claude-3-7-sonnet-20250219",
-                temperature = 0,
-                max_tokens = 4096,
-                -- disable_tools = true,
+            auto_suggestions_provider = "copilot", -- 一応設定しておく
+            file_selector = {
+                provider = "telescope",
+            },
+            behaviour = {
+                auto_suggestions = false, -- 試験的機能につき無効化を推奨
+                auto_set_highlight_group = true,
+                auto_set_keymaps = true,
+                auto_apply_diff_after_generation = false,
+                support_paste_from_clipboard = true,
+                minimize_diff = true,
             },
         },
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -42,7 +58,7 @@ return {
             "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
             "ibhagwan/fzf-lua", -- for file_selector provider fzf
             "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+            "zbirenbaum/copilot.lua", -- for providers='copilot'
             {
                 -- support for image pasting
                 "HakonHarnes/img-clip.nvim",
